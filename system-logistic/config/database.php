cd31b4b5fecbbee4b09d9e4cafc977a5c2196acc
Daniel Ayala <50875171+daniayala@users.noreply.github.com>
2022-11-11 18:13:06 -0300

    primer deploy

---
+++ system-logistic/config/database.php
@@ -0,0 +1,11 @@
+<?php
+    return array(
+        "driver"   => "mysql",
+        "host"     => "containers-us-west-50.railway.app:7167",
+        "user"     => "root",
+        "pass"     => "QRFFZMt2h7tEegnkGHNw",
+        "database" => "railway",
+        "charset"  => "utf8"
+
+    );
+?>
